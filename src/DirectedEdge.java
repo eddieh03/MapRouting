b45644f49528c408a3217c26747683126e0e6e88
Edonë Haziri <edonehaziri@gmail.com>
2023-01-26 23:22:49 +0100

    Initial Commit

---
+++ src/DirectedEdge.java
@@ -0,0 +1,71 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+
+public class DirectedEdge { 
+    private final int v;
+    private final int w;
+    private final double weight;
+    private boolean rushHour;
+
+    /**
+     * Initializes a directed edge from vertex {@code v} to vertex {@code w} with
+     * the given {@code weight}.
+     * @param v the tail vertex
+     * @param w the head vertex
+     * @param weight the weight of the directed edge
+     * @throws IllegalArgumentException if either {@code v} or {@code w}
+     *    is a negative integer
+     * @throws IllegalArgumentException if {@code weight} is {@code NaN}
+     */
+    public DirectedEdge(int v, int w, double weight,boolean rushHour) {
+        if (v < 0) throw new IllegalArgumentException("Vertex names must be non-negative integers");
+        if (w < 0) throw new IllegalArgumentException("Vertex names must be non-negative integers");
+        if (Double.isNaN(weight)) throw new IllegalArgumentException("Weight is NaN");
+        this.v = v;
+        this.w = w;
+        this.weight = weight;
+        this.rushHour = rushHour;
+        
+    }
+    
+    public boolean rushHour(){
+        return rushHour;
+    }
+
+    /**
+     * Returns the tail vertex of the directed edge.
+     * @return the tail vertex of the directed edge
+     */
+    public int from() {
+        return v;
+    }
+
+    /**
+     * Returns the head vertex of the directed edge.
+     * @return the head vertex of the directed edge
+     */
+    public int to() {
+        return w;
+    }
+
+    /**
+     * Returns the weight of the directed edge.
+     * @return the weight of the directed edge
+     */
+    public double weight() {
+        return weight;
+    }
+
+    /**
+     * Returns a string representation of the directed edge.
+     * @return a string representation of the directed edge
+     */
+    public String toString() {
+        return v + "->" + w + " " + String.format("%5.2f", weight);
+    }
+
+   
+}
